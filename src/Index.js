7dbd414e3b59d1bbe2695aac8ff4e59e514a975f
Alok Mourya <alokmourya44@gmail.com>
2025-03-16 23:25:15 +0530

    connect express

--- src/Index.js
+++ src/Index.js
@@ -4,4 +4,11 @@ import connectDB from "./db/index.js";
 dotenv.config({ path: './.env' });
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 6000 ,()=>{
+    console.log("mongodb database run successfully")
+    })
+})
+.catch((Error)=>{
+    console.log("database connection failed",Error)
+})
