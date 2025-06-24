c4421c66e6afb9a73ac34cf867209a9026c07e76
Alok Mourya <alokmourya44@gmail.com>
2025-06-24 22:38:19 +0530

    Add user profile and watch history retrieval endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import uploadONCloudinary from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose, { mongo } from "mongoose";
 
 // Function to generate access and refresh tokens for a user
 // This function takes a userId, finds the user, generates tokens, saves the refresh token, and returns both tokens.
@@ -283,6 +284,122 @@ const updateUserCover = asyncHandler(async (req, res) => {
 
 })
 
+const getChennelUserProfile = asyncHandler(async (req, res) => {
+
+    const { username } = req.params
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is required")
+    }
+
+    const chennel = await User.aggregate([
+        {
+            $match: {
+                username: username.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "Subscription",
+                localField: "_id",
+                foreignField: "chennels",
+                as: "subcribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "Subscription",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedChennels"
+            }
+        },
+        {
+            $addFields: {
+                subcribersCount: { $size: "$subcribers" },
+                subscribedChennelsCount: { $size: "$subscribedChennels" },
+                issubcriberd: {
+                    $cond: {
+                        if: { $in: [req.user._id, "$subcribers.subscriber"] },
+                        then: true,
+                        else: false,
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullname: 1,
+                lastname: 1,
+                username: 1,
+                email: 1,
+                avatar: 1,
+                cover: 1,
+                subcribersCount: 1,
+                subscribedChennelsCount: 1,
+            }
+        }
+    ])
+
+    if (!chennel || chennel.length === 0) {
+        throw new ApiError(404, "Chennel does not exist")
+    }
+
+    return res.status(200)
+        .json(new ApiResponse(200, chennel[0], "Chennel user profile retrieved successfully"))
+
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "video",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "user",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+
+        },
+        {
+            $addFields: {
+                owner: {
+                    $first: "$owner"
+                }
+            }
+        }
+    ])
+
+    return res.status(200)
+        .json(new ApiResponse(200, user[0].watchHistory, "Watch history retrieved successfully"))
+})
+
+
 export {
     userRegister,
     loginUser,
@@ -293,4 +410,6 @@ export {
     changeAccountDetails,
     updateUserAvatar,
     updateUserCover,
+    getChennelUserProfile,
+    getWatchHistory
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, userRegister,logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    userRegister,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changePassword,
+    getCurrentUser,
+    changeAccountDetails,
+    updateUserAvatar,
+    updateUserCover,
+    getChennelUserProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 import { verifJWT } from "../middlewares/auth.middlewares.js"
 
@@ -7,18 +19,25 @@ import { verifJWT } from "../middlewares/auth.middlewares.js"
 const router = Router()
 
 router.route("/register").post(
-upload.fields([
-    { name: 'avatar', maxCount: 1 },
-    { name: 'cover', maxCount: 1 },
-]) ,  
+    upload.fields([
+        { name: 'avatar', maxCount: 1 },
+        { name: 'cover', maxCount: 1 },
+    ]),
     userRegister
 );
 
 router.route("/login").post(loginUser)
 
 // secured routes
-router.route("/logout").post(verifJWT,logoutUser)
+router.route("/logout").post(verifJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifJWT, changePassword)
+router.route("/current-user").get(verifJWT, getCurrentUser)
+router.route("/update-user").patch(verifJWT, changeAccountDetails)
+router.route("/update-avatar").patch(verifJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-cover").patch(verifJWT, upload.single("cover"), updateUserCover)
+router.route("/c/:username").get(verifJWT, getChennelUserProfile)
+router.route("/history").get(verifJWT, getWatchHistory)
 
 export default router;
